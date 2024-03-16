3f4dfd8e7c0651188a2bcc052a2b3528aea469b9
Tristan Tarpley <me@tristantarpley.com>
2024-03-16 18:30:57 -0500

    typography, links, and buttons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
+import * as palette from './palette.mjs';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				...palette,
+			},
+			fontFamily: {
+				sans: ["Schibsted Grotesk", "sans-serif"],
+				serif: ["Bitter", "serif"],
+				mono: ["Sometype Mono", "monospace"],
+			}
+		},
 	},
 	plugins: [],
 }
